033ed16899a915abd70b1dba80928100156c0a7b
mmoh-scalr-1904 <104007136+mmoh-scalr-1904@users.noreply.github.com>
2022-04-19 15:50:35 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,3 @@
-output "very_long" {
-  value = "КИРИЛЛИЦА Lorem dfipsusdm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-  description = "Both Latin and Cyrillic placeholder text"  
-}
-
 output "sens_output" {
   value = "secret data"
   description = "Sensitive output description"
