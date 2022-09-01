d6fc9a7f563e920c57fb29780bb59fd925ff8df3
mmoh-scalr-1904 <104007136+mmoh-scalr-1904@users.noreply.github.com>
2022-09-01 19:10:49 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,15 +3,3 @@ output "sens_output" {
   description = "Sensitive output description"
   sensitive = true
 }
-
-output "pet_name" {
-  value = random_pet.pet.id
-  description = "Pet's name"
-  sensitive = false
-}
-
-output "pet_age" {
-  value = random_integer.age.result
-  description = "Pet's age"
-  sensitive = false
-}
